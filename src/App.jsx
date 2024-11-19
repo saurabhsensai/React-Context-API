23d778362016c59a609a727a93e6262b1bc269a4
saurabhsensai <saurabhnale9@gmail.com>
2024-11-19 13:04:54 +0530

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+
+function App() {
+ 
+  return (
+    <UserContextProvider>
+     <Login></Login>
+     <Profile></Profile>
+    </UserContextProvider>
+  )
+}
+
+export default App
